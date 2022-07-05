9916a3fa981b12fe39918cdc778f505d1bd5614c
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-07-05 23:32:27 +0300

    feat: Modify the display table

--- src/components/DisplayTable.jsx
+++ src/components/DisplayTable.jsx
@@ -1,27 +1,45 @@
-import { DataGrid } from "@mui/x-data-grid";
+import MUIDataTable from "mui-datatables";
 
 const columns = [
-	{ field: "name", headerName: "Name", width: 230 },
-	{ field: "email", headerName: "Email", width: 400 },
 	{
-		field: "phone",
-		headerName: "Phone",
-		type: "number",
-		width: 210
+		name: "name",
+		label: "Name",
+		options: {
+			filter: true,
+			sort: true
+		}
+	},
+	{
+		name: "email",
+		label: "Email",
+		options: {
+			filter: true,
+			sort: false
+		}
+	},
+	{
+		name: "phone",
+		label: "Phone",
+		options: {
+			filter: true,
+			sort: false
+		}
 	}
 ];
 
+const options = {
+	responsive: 'simple',
+	rowsPerPage: 10,
+	selectableRows: false
+};
 
 export const DisplayTable = ({rows}) => {
-    console.log(rows)
 	return (
-		<div style={{ height: 480 }}>
-			<DataGrid
-				rows={rows}
-				columns={columns}
-				pageSize={10}
-				rowsPerPageOptions={[5]}
-			/>
-		</div>
+		<MUIDataTable
+			title={"CSV File Data"}
+			data={rows}
+			columns={columns}
+			options={options}
+		/>
 	);
 };
