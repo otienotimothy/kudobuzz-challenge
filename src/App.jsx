b185e62ac954b69130fd4f1167ff7543ee5a3e13
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-07-05 18:56:14 +0300

    feat:modify the csv data into an array of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import { useState } from "react"
 import { UploadCSV } from "./components/UploadCSV"
 
 function App() {
+  const [csvFile, setCsvFile] = useState();
+	const [error, setError] = useState("");
+  const [data, setData] = useState([])
+
+  const buildCsvData = (str) => {
+		const tableHeaders = str.slice(0, str.indexOf("\n") - 1).split(",");
+		const tableRows = str.slice(str.indexOf("\n") + 1).split("\n");
+		console.log(tableHeaders);
+		console.log(tableRows);
+
+		const dataArray = tableRows.map((row) => {
+			let values = row.split(",");
+			let dataObj = tableHeaders.reduce((obj, header, i) => {
+				obj[header] = values[i];
+				return obj;
+			}, {});
+			return dataObj;
+		});
+    console.log(dataArray)
+    setData(dataArray)
+	};
+
+  const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (csvFile) {
+			const reader = new FileReader();
+
+			reader.onload = (e) => {
+				const text = e.target.result;
+				console.log(text);
+				buildCsvData(text);
+			};
+			reader.readAsText(csvFile);
+		} else {
+			setError("Please Upload a CSV File");
+		}
+	};
+
+  console.log(data.map(item => item.name))
 
   return (
-    <UploadCSV />
+    <>
+     <UploadCSV setFile={setCsvFile} handleSubmit={handleSubmit} error={error} />
+     {data && data.map(item => <p> {item.name} </p>)}
+    </>
   )
 }
 
--- src/components/UploadCSV.jsx
+++ src/components/UploadCSV.jsx
@@ -1,29 +1,17 @@
-import { useState } from "react";
-
-export const UploadCSV = () => {
-	const [csvFile, setCsvFile] = useState();
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const reader = new FileReader()
-
-        reader.onload = (e) => {
-            const text = e.target.result;
-            console.log(text)
-        }
-
-        reader.readAsText(csvFile)
-    }
+export const UploadCSV = ({setFile, handleSubmit, error}) => {
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				type="file"
-				accept=".csv"
-				name="csv"
-				onChange={(e) => setCsvFile(e.target.files[0])}
-			/>
-			<button>Submit</button>
-		</form>
+		<>
+			{error ? <p> {error} </p> : null}
+			<form onSubmit={handleSubmit}>
+				<input
+					type="file"
+					accept=".csv"
+					name="csv"
+					onChange={(e) => setFile(e.target.files[0])}
+				/>
+				<button>Submit</button>
+			</form>
+		</>
 	);
 };
